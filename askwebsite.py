8bdc68428130f8c31885a0bf40707f7b53bd489e
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-09-04 13:25:49 +0200

    Update askwebsite.py

--- askwebsite.py
+++ askwebsite.py
@@ -17,3 +17,5 @@ for num in data:
 #I want to search for Under Armour CURRY 10 on czech websites, Aliexpress, etc.
 # Sources:
 https://www.zoot.cz/polozka/3606790/oranzovo-cerne-panske-tenisky-under-armour-curry-10
+
+#poohlednout se po webscrapping videich, udelat si silny uvod, atd. tzn. Udemy, Chatgpt, atd.
